7281477d7814d5f4bf97a6e56107948dbfe627ca
boyakafp <boyakafp@miamioh.edu>
2018-03-01 22:27:06 -0500

    Message

--- src/Calc.java
+++ src/Calc.java
@@ -1,7 +1,23 @@
-public class Calc
-{
-   static public int add (int a, int b)
-   {
-      return a + b;
-   }
+// Authors: Florian Boyaka and Elinam Gbordzoe
+
+public class Calc {
+	static public int add(int a, int b) {
+		return a + b;
+	}
+
+	static public int substract(int a, int b) {
+		//return a - b;
+		return -a - b;
+	}
+
+	static public int multiply(int a, int b) {
+		//return a * b;
+		return b - (a * b);
+	}
+
+	static public double divide(int a, int b) {
+		if(b == 0)
+			throw new ArithmeticException();
+		return (double)a/(double)b;
+	}
 }
--- src/CalcTester.java
+++ src/CalcTester.java
@@ -3,11 +3,103 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalcTester {
-
+	Calc calc;
+	
+	// ============================================= Subtraction
+	/*@Test
+	public void negativeSecondValueSubtraction() {
+		int firstVal = 9;
+		int secondVal = -5;
+		int result = Calc.substract(firstVal, secondVal);
+		assertTrue("Incorrect subtraction for the second negative integer", result == 14);
+	}
+	
 	@Test
-	public void calcFail() {
-		Calc c = new Calc();
-		assertTrue(c.add(1, 1)==3);
+	public void positiveSecondValue() {
+		int firstVal = 0;
+		int secondVal = 4;
+		int result = Calc.substract(firstVal, secondVal);
+		assertTrue("Incorrect subtraction for a positive second integer", result == -4);
+	}
+	
+	@Test
+	public void zeroSecondValue() {
+		int firstVal = 7;
+		int secondVal = 0;
+		int result = Calc.substract(firstVal, secondVal);
+		assertTrue("Incorrect subtraction for a positive second integer", result == firstVal);
+	}*/
+
+	// ============================================= Multiplication
+	@Test
+	public void bothPosMultiplication() {
+		int firstVal = 1;
+		int secondVal = 3;
+		int result = Calc.multiply(firstVal, secondVal);
+		assertTrue("Incorrect multiplication for two positve integers", result > 0);
 	}
 
+	@Test
+	public void bothNegMultiplication() {
+		int firstVal = -4;
+		int secondVal = -9;
+		int result = Calc.multiply(firstVal, secondVal);
+		assertTrue("Incorrect mulitplication for two negative integers", result > 0);
+	}
+
+	@Test
+	public void onePosOneNegMultiplication() {
+		int firstVal = -2;
+		int secondVal = 3;
+		int result = Calc.multiply(firstVal, secondVal);
+		assertTrue("Incorrect multiplication of one positive and one negative integer", result < 0);
+	}	
+	
+	@Test
+	public void zeroValueMultiplication() {
+		int firstVal = 0;
+		int secondVal = 5;
+		int result = Calc.multiply(firstVal, secondVal);
+		assertTrue("Incorrect multiplication of zero and any integer", result == 0);
+	}
+
+	// ============================================= Division
+	/*@Test
+	public void bothPosDivision() {
+		int firstVal = 1;
+		int secondVal = 4;
+		double result = Calc.divide(firstVal, secondVal);
+		assertTrue("Incorrect division for two positve integers", result > 0);
+	}
+	
+	@Test
+	public void bothNegDivision() {
+		int firstVal = -8;
+		int secondVal = -4;
+		double result = Calc.divide(firstVal, secondVal);
+		assertTrue("Incorrect division for two negative integers", result > 0);
+	}
+	
+	@Test
+	public void onePosOneNegDivision() {
+		int firstVal = 36;
+		int secondVal = -4;
+		double result = Calc.divide(firstVal, secondVal);
+		assertTrue("Incorrect division for a positve and negative integer", result < 0);
+	}	
+
+	@Test
+	public void zeroValueDivision() {
+		int firstVal = 0;
+		int secondVal = 8;
+		double result = Calc.divide(firstVal, secondVal);
+		assertTrue("Incorrect division for zero and any integer", result == 0);
+	}
+	
+	@Test (expected = ArithmeticException.class)
+	public void zeroInDenominator() {
+		int firstVal = 3; 
+		int secondVal = 0;
+		double result = Calc.divide(firstVal, secondVal);
+	}*/
 }
